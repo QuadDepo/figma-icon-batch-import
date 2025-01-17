79d557ae1e78f58436887fc39798e8a595192035
Ryan George <Ryan@qdds.nl>
2025-01-17 15:24:58 +0100

    chore: adjust grid styling

--- src/ui/components/grid/Grid.tsx
+++ src/ui/components/grid/Grid.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 const Grid: FC<Props> = ({ items }) => {
 	return (
-		<div className="grid grid-cols-6 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-12">
+		<div className="grid grid-cols-6 md:grid-cols-8 lg:grid-cols-10 p-4 xl:grid-cols-12 gap-4">
 			{items.map(([title, svg]) => (
-				<GridItem title={title} svg={svg} />
+				<GridItem key={title} title={title} svg={svg} />
 			))}
 		</div>
 	);
--- src/ui/components/grid/GridItem.tsx
+++ src/ui/components/grid/GridItem.tsx
@@ -7,9 +7,8 @@ type Props = {
 
 const GridItem: FC<Props> = ({ svg }) => {
 	return (
-		<div className="p-2 md:p-4 border -mb-px -mr-px  border-gray-300">
+		<div className="p-2 border border-gray-300 rounded-md shadow-sm">
 			<div
-				className="p-4"
 				dangerouslySetInnerHTML={{
 					__html: svg,
 				}}
